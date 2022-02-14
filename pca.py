a1a3d001e0c5a672b831d5a2b77ad3cc3666eb3b
chunyan.mao <chunyan.mao@outlook.com>
2022-02-14 14:53:55 +0800

    add PCA kmeans on iris data

---
+++ pca.py
@@ -0,0 +1,81 @@
+from sklearn import datasets
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.metrics import calinski_harabasz_score
+from sklearn.neighbors import LocalOutlierFactor
+
+
+# 下载数据集
+iris = datasets.load_iris()
+x = iris.data
+y = iris.target
+target_names = iris.target_names
+
+# 0、离群点检测，获取正确的样本
+print("去除离群点之前样本大小：{}".format(len(x)))
+estimator = LocalOutlierFactor(n_neighbors= 5).fit_predict(list(x))
+# 取出正样本
+estimator = [bool(i) for i in estimator]
+x = x[estimator]
+y = y[estimator]
+print("去除离群点后样本大小：{}".format(len(x)))
+
+# 一、根据PCA映射的二维图形状，选择合适的聚类算法及聚类数目
+reduced = PCA(n_components = 2).fit_transform(x)
+# 画出二维数据图
+plt.subplot(221)
+plt.scatter(reduced[:,0],reduced[:,1])
+plt.title("feature image")
+# result:型状可用kmeans，cluster选择2或者3
+
+# 使用Kmeans
+# 这里用原来的数据聚类，还是用降维后的数据聚类？
+# 二、kmeans的评估：1、elbow 2、轮廓系数 3、
+# 1、elbow
+len(x)
+inertias = []
+for cluster in range(2,30):
+    kmeans = KMeans(n_clusters= cluster).fit(reduced)
+    inertias.append(kmeans.inertia_)
+plt.subplot(222)
+plt.plot(list(range(2,30)),inertias)
+plt.scatter(list(range(2,30)),inertias)
+#plt.title("different cluster num")
+plt.title("elbow image")
+# result:
+
+# 2、轮廓系数
+silhouette=[]
+for cluster in range(2,30):
+    kmeans = KMeans(n_clusters=cluster).fit(reduced)
+    t = silhouette_score(x,kmeans.labels_)
+    silhouette.append(t)
+plt.subplot(223)
+plt.plot(range(2,30),silhouette)
+plt.scatter(range(2,30),silhouette)
+
+#plt.title("different cluster num")
+plt.xlabel("silhouette image")
+#result:
+
+# 3、cal_score
+ch_score = []
+for cluster in range(2,30):
+    kmeans = KMeans(n_clusters=cluster).fit(reduced)
+    t = calinski_harabasz_score(x,kmeans.labels_)
+    ch_score.append(t)
+plt.subplot(224)
+plt.plot(list(range(2,30)),ch_score)
+plt.scatter(list(range(2,30)),ch_score)
+#plt.xlabel("different cluster num")
+plt.xlabel("calinski_harabasz_score  image")
+# result：
+plt.show()
+
+# 确认cluster，得出聚类结果
+kmeans = KMeans(n_clusters=3).fit(reduced)
+print(kmeans.labels_)
